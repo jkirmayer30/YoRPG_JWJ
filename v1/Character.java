c5ee2ae70e791ea3dd3c773431662a16d87976a1
thunder-red-star <wvongphanith30@stuy.edu>
2021-11-23 12:32:53 -0500

    add packages

--- v1/Character.java
+++ v1/Character.java
@@ -1,3 +1,5 @@
+package v1;
+
 public class Character {
   public String _name;
   public int _health;
@@ -10,10 +12,33 @@ public class Character {
     _damageMult = 1;
   }
 
-  public Character(String name) {
+  public Character(
+          String name
+  ) {
     _name = name;
     _health = 50;
     _baseDamage = 10;
+    _damageMult = 1;
+  }
+
+  public Character(
+          String name,
+          int health
+  ) {
+    _name = name;
+    _health = health;
+    _baseDamage = 10;
+    _damageMult = 1;
+  }
+
+  public Character(
+          String name,
+          int health,
+          int baseDamage
+  ) {
+    _name = name;
+    _health = health;
+    _baseDamage = baseDamage;
     _damageMult = 1;
   }
 
@@ -33,7 +58,7 @@ public class Character {
     if (Math.random()>0.5){
       _health -= damage*_damageMult;
       if (_damageMult ==0){
-        System.out.println("damage mult 0");
+        System.out.println("You did no damage. You missed");
       }
       System.out.println(_damageMult);
       return damage*_damageMult;
