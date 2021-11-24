6352b4cb7035aeb8df8c6b7a12899e6d98201491
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-24 00:47:36 -0500

    Create Assassin.java

---
+++ v3/Assassin.java
@@ -0,0 +1,31 @@
+public class Assassin extends Protagonist {
+  public String _name;
+  public int _baseDamage;
+
+  public Assassin(String name) {
+    _name = name;
+    _health = 100;
+    _baseDamage = 20;
+    _damageMult = 1;
+    _defense = 10;
+  }
+
+  public static String about() {
+    return "Never misses an attack.";
+  }
+
+  public void specialize() {
+    this._damageMult = 3;
+  }
+
+  public void normalize() {
+    this._damageMult = 2;
+  }
+
+  public int attack(Character m) {
+    int totalDamage = 0;
+    totalDamage = _baseDamage * _damageMult - _defense;
+    return m.reduceHealth(totalDamage);
+  }
+
+}
