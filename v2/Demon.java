4c7e42893d3ed017f517e1be645526010d83e2d3
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-23 23:19:06 -0500

    Create Demon.java

---
+++ v2/Demon.java
@@ -0,0 +1,12 @@
+public class Demon extends Monster {
+  public String _name;
+  public int _baseDamage;
+
+  public Demon() {
+    _health = 120;
+    _baseDamage = 2;
+    _damageMult = 2;
+    _defense = 10;
+  }
+
+}
