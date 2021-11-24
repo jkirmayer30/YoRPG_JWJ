4b05d8cdf420a9d45084c1585b782f8f6521fb0a
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-24 00:48:20 -0500

    Create GoblinKing.java

---
+++ v3/GoblinKing.java
@@ -0,0 +1,15 @@
+public class GoblinKing extends Monster {
+  public String _name;
+  public int _baseDamage;
+
+  public String about() {
+    return "He just swings his club and sends the goblins flying.";
+  }
+
+  public GoblinKing() {
+    super();
+    _health = 350;
+    _baseDamage = 30; 
+  }
+
+}
