0536ba08e5bb727afbb39ccf943a769ec03ba703
thunder-red-star <wvongphanith30@stuy.edu>
2021-11-24 12:26:38 -0500

    it wprks

--- v3/Monster.java
+++ v3/Monster.java
@@ -1,7 +1,10 @@
 public class Monster extends Character {
   public int _baseDamage;
+  public int _damageMult;
+  public int _defense;
 
   public Monster() {
+    super();
     _health = 300;
     _baseDamage = 20;
     _damageMult = 1;
