41cc796d64fcf41746a389d6019cae5bd4e1fccf
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-24 01:13:12 -0500

    Update Monster.java

--- v3/Monster.java
+++ v3/Monster.java
@@ -3,7 +3,10 @@ public class Monster extends Character {
   public int _baseDamage;
 
   public Monster() {
-    super();
+    _health = 300;
+    _baseDamage = 20;
+    _damageMult = 1;
+    _defense = 10;
   }
 
 }
