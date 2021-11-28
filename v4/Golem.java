dea174237ac5dcae47309363813851c3598cbffa
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-27 22:32:17 -0500

    Update Golem.java

--- v4/Golem.java
+++ v4/Golem.java
@@ -1,11 +1,17 @@
 public class Golem extends Monster {
-  public String about() {
-    return "This guy's a tank. Literally a wall made of stone. ";
+  public int _health;
+
+  public static String about() {
+    return "a wall that ran away after it was built. Literally a wall of stone, very tanky.";
   }
 
   public Golem() {
     super();
     _health = 400;
+  }
+
+  public String getRole () {
+    return "Golem";
   }
 
 }
