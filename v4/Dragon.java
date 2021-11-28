1f8f838b4c769f9b21dc55c85c0816266b70f254
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-27 22:31:33 -0500

    Update Dragon.java

--- v4/Dragon.java
+++ v4/Dragon.java
@@ -1,12 +1,17 @@
 public class Dragon extends Monster {
-  public String about() {
-    return "Has strong scales. Swords and sticks are not effective. ";
+  public int _defense;
+
+  public static String about() {
+    return "a flying mythical creature of mass destruction. Its strong scales give it tough defense. ";
   }
 
   public Dragon() {
     super();
-    _health = 300;
-    _defense = 20; 
+    _defense = 20;
+  }
+
+  public String getRole () {
+    return "Dragon";
   }
 
 }
