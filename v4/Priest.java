d6ad61fa933e11b433720bed78b76debe04f4b53
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-27 22:30:54 -0500

    Update Priest.java

--- v4/Priest.java
+++ v4/Priest.java
@@ -2,11 +2,14 @@ public class Priest extends Protagonist {
 
   public Priest(String name) {
     super(name);
-    _defense = 15; // 5 added to defense is heals
+    _defense = 10;
   }
 
   public static String about() {
-    return "Heals for 5 health after every round.";
+    return "A messenger of God with powerful prayers. Heals self for 10 health after every round, but has decreased defense. Specialized attacks deal double damage.";
   }
 
+  public String getRole () {
+    return "Priest";
+  }
 }
