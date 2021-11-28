7c796de4a9ea4f6871eadc7145cd3912869a97ab
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-27 22:31:12 -0500

    Update Assassin.java

--- v4/Assassin.java
+++ v4/Assassin.java
@@ -4,17 +4,30 @@ public class Assassin extends Protagonist {
 
   public Assassin(String name) {
     super(name);
-    _baseDamage = 20;
+    _baseDamage = 20; 
   }
 
   public static String about() {
-    return "A marksman with a bow who never misses an attack. Special attacks have a 10% chance of dealing 10x damage.";
+    return "A marksman with a bow who never misses an attack. Specialized attacks have a 10% chance of dealing 5x damage.";
   }
 
   public int attack(Character m) {
     int totalDamage = 0;
     totalDamage = _baseDamage * _damageMult - _defense;
     return m.reduceHealth(totalDamage);
+  }
+
+
+  public void specialize() {
+    if ((int) (Math.random() * 10) < 1 ) { // 10% chance
+      this._damageMult = 5; // 10x damage
+    } else {
+      this._damageMult = 1; // 90% chance no damage
+    }
+  }
+
+  public String getRole () {
+    return "Assassin";
   }
 
 }
